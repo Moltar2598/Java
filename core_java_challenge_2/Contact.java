af4ad62bd097f439935bde8e503683fa95a25c9a
M. Talha <42629783+Moltar2598@users.noreply.github.com>
2022-04-16 10:12:15 +0530

    contact and messages
    OOPs challenge 2
    1. Adding, deleting, or display of contact.
    2. Sending a message, or displaying the message to/of the contacts added.

---
+++ core_java_challenge_2/Contact.java
@@ -0,0 +1,62 @@
+package core_java_challenge_2;
+
+import java.util.ArrayList;
+
+public class Contact {
+    private String name;
+    private String number;
+    private String email;
+    private ArrayList<Message> message;
+
+    public Contact(String name, String number, String email) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.message = new ArrayList<>();
+    }
+
+    public Contact(String name, String number, String email, ArrayList<Message> message) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.message = message;
+    }
+
+    public void getDetails(){
+        System.out.println("Name: "+this.name+"\n"+
+                "Number: "+this.number+"\n"+
+                "Email: "+this.email+"\n");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Message> getMessage() {
+        return message;
+    }
+
+    public void setMessage(ArrayList<Message> message) {
+        this.message = message;
+    }
+}
